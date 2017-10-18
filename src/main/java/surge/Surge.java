ca47b20adbacec7b7a6d0b542897446a14971160
Dan <cyberpwnn@users.noreply.github.com>
2017-10-18 19:15:47 -0400

    Surge leeching system

--- src/main/java/surge/Surge.java
+++ src/main/java/surge/Surge.java
@@ -1,6 +1,34 @@
 package surge;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
 public class Surge
 {
+	protected static PluginLeech leech = null;
 
+	public static PluginLeech beginLeeching(Plugin plugin)
+	{
+		return new PluginLeech(plugin);
+	}
+
+	public static boolean isMainThread()
+	{
+		return Bukkit.isPrimaryThread();
+	}
+
+	protected static void stopLeeching()
+	{
+		leech = null;
+	}
+
+	public static boolean hasLeech()
+	{
+		return getLeech() != null && getLeech().isConnected();
+	}
+
+	public static PluginLeech getLeech()
+	{
+		return leech;
+	}
 }
