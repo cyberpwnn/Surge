e0ecf8d592281561b6773209584cefea4c2877b0
Dan <cyberpwnn@users.noreply.github.com>
2017-10-18 18:41:47 -0400

    Base cluster

---
+++ src/main/java/surge/cluster/Cluster.java
@@ -0,0 +1,31 @@
+package surge.cluster;
+
+public class Cluster<T> implements ICluster<T>
+{
+	private ClusterType type;
+	private T t;
+
+	protected Cluster(ClusterType type, T t)
+	{
+		this.type = type;
+		set(t);
+	}
+
+	@Override
+	public ClusterType getType()
+	{
+		return type;
+	}
+
+	@Override
+	public void set(T t)
+	{
+		this.t = t;
+	}
+
+	@Override
+	public T get()
+	{
+		return t;
+	}
+}
