1ef04ce412e1a64a01df513893986f2f252686dd
Dan <cyberpwnn@users.noreply.github.com>
2017-10-19 07:11:22 -0400

    vc

--- src/main/java/surge/TestPlugin.java
+++ src/main/java/surge/TestPlugin.java
@@ -1,6 +1,9 @@
 package surge;
 
+import java.io.File;
+
 import surge.control.AmpedPlugin;
+import surge.sched.Task;
 import surge.util.D;
 import surge.util.Protocol;
 
@@ -9,7 +12,65 @@ public class TestPlugin extends AmpedPlugin
 	@Override
 	public void onStart(Protocol serverProtocol)
 	{
-		D.v("Plugin onStart()");
+		D.v("Minecraft Version: " + serverProtocol.toString());
+
+		// Get your plugin instance anywhere
+		Surge.getAmp().getPluginInstance();
+
+		// Track a file
+		Surge.getHotloadManager().track(new File("plugins/SomeJar.jar"), new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				// This is already checked for your plugin jar
+				// Surge automatically reloads when it's recompiled
+				D.w("File was modified!");
+			}
+		});
+
+		// Logging
+		D.l(this, "Simple logging, with instance ref");
+		D.l("info");
+		D.v("verbose");
+		D.f("fail");
+		D.w("warning");
+		D.s("success");
+
+		// Tasks
+		new Task("update-check?")
+		{
+			@Override
+			public void run()
+			{
+				D.v("fires once");
+			}
+		};
+
+		// Repeating Tasks
+		new Task("repeat-every-tick", 0)
+		{
+			@Override
+			public void run()
+			{
+				D.v("Run Time: " + getActiveTime());
+				D.v("Compute Time: " + getComputeTime());
+				D.v("Total Compute Time: " + getTotalComputeTime());
+
+				// cancel it anywhere any time
+				cancel();
+			}
+		};
+
+		// Repeating Tasks with limits
+		new Task("repeat every second for 10 seconds", 20, 10)
+		{
+			@Override
+			public void run()
+			{
+				D.v("Tick!");
+			}
+		};
 	}
 
 	@Override
