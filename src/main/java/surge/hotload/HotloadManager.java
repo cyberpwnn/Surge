32609ed2658f2d63b169fd28db5f77bc45d1c7a0
Dan <cyberpwnn@users.noreply.github.com>
2017-10-19 06:30:28 -0400

    hotload manager

---
+++ src/main/java/surge/hotload/HotloadManager.java
@@ -0,0 +1,70 @@
+package surge.hotload;
+
+import java.io.File;
+
+import surge.collection.GMap;
+import surge.sched.IMasterTickComponent;
+import surge.util.D;
+
+public class HotloadManager implements IMasterTickComponent
+{
+	private GMap<File, Long> filemods;
+	private GMap<File, Long> filesizes;
+	private GMap<File, Runnable> fileacts;
+
+	public HotloadManager()
+	{
+		filemods = new GMap<File, Long>();
+		filesizes = new GMap<File, Long>();
+		fileacts = new GMap<File, Runnable>();
+	}
+
+	public void track(File file, Runnable r)
+	{
+		filemods.put(file, file.lastModified());
+		filesizes.put(file, file.length());
+		fileacts.put(file, r);
+		D.v("Now tracking for hotloads: " + file.getAbsolutePath());
+	}
+
+	public void untrack(File f)
+	{
+		filemods.remove(f);
+		filesizes.remove(f);
+		fileacts.remove(f);
+	}
+
+	public void untrackall()
+	{
+		filemods.clear();
+		filesizes.clear();
+		fileacts.clear();
+	}
+
+	@Override
+	public void onTick()
+	{
+		for(File i : filemods.k())
+		{
+			if(i.exists() && (i.lastModified() != filemods.get(i) || i.length() != filesizes.get(i)))
+			{
+				D.v(i.getName() + " modified");
+				fileacts.get(i).run();
+				filemods.put(i, i.lastModified());
+				filesizes.put(i, i.length());
+			}
+
+			if(!i.exists() || i.isDirectory())
+			{
+				D.v(i.getName() + " deleted, untracking.");
+				untrack(i);
+			}
+		}
+	}
+
+	@Override
+	public String getTickName()
+	{
+		return "hotloadmgr";
+	}
+}
