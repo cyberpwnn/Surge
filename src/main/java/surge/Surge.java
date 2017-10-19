492403357e90835be03aeb9ec5a89e4ead8fe7f7
Dan <cyberpwnn@users.noreply.github.com>
2017-10-19 06:30:53 -0400

    Surge hotload mgr

--- src/main/java/surge/Surge.java
+++ src/main/java/surge/Surge.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
 import surge.collection.GList;
+import surge.hotload.HotloadManager;
 import surge.sched.IMasterTickComponent;
 import surge.sched.TaskManager;
 
@@ -12,10 +13,17 @@ public class Surge
 	protected static PluginAmp amp = null;
 	private static GList<IMasterTickComponent> tickComponents = new GList<IMasterTickComponent>();
 	private static TaskManager taskmgr;
+	private static HotloadManager hotloadmgr;
 
 	private static void startup()
 	{
 		registerTicked(taskmgr = new TaskManager());
+		registerTicked(hotloadmgr = new HotloadManager());
+	}
+
+	public static GList<IMasterTickComponent> getTickComponents()
+	{
+		return tickComponents;
 	}
 
 	public static void registerTicked(IMasterTickComponent tick)
@@ -56,6 +64,11 @@ public class Surge
 	public static TaskManager getTaskManager()
 	{
 		return taskmgr;
+	}
+
+	public static HotloadManager getHotloadManager()
+	{
+		return hotloadmgr;
 	}
 
 	static
