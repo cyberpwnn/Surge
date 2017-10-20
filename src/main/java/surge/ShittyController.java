d99d6567a01a0fa6b02481bd246e6961f455ef50
Dan <cyberpwnn@users.noreply.github.com>
2017-10-20 13:21:02 -0400

    Utils

---
+++ src/main/java/surge/ShittyController.java
@@ -0,0 +1,29 @@
+package surge;
+
+import surge.control.Controller;
+import surge.util.D;
+
+public class ShittyController extends Controller
+{
+
+	@Override
+	public void start()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void tick()
+	{
+		D.l(this, "Ticked");
+	}
+
+}
