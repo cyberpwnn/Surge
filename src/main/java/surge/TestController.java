d2663cfe1caf0c7958618a1f38a5afa5f5f85b6f
Dan <cyberpwnn@users.noreply.github.com>
2017-10-20 08:24:57 -0400

    Test sampler

--- src/main/java/surge/TestController.java
+++ src/main/java/surge/TestController.java
@@ -1,36 +1,32 @@
 package surge;
 
 import surge.control.Controller;
-import surge.pool.A;
+import surge.server.SuperSampler;
 import surge.util.D;
+import surge.util.F;
 
 public class TestController extends Controller
 {
+	private SuperSampler ss;
+
 	@Override
 	public void start()
 	{
 		D.v(this, "Controller start()");
+		ss = new SuperSampler();
+		ss.start();
 	}
 
 	@Override
 	public void stop()
 	{
 		D.v(this, "Controller stop()");
+		ss.stop();
 	}
 
 	@Override
 	public void tick()
 	{
-		D.v(this, "Controller tick() ");
-		D.l(this, "There are " + Surge.getAmp().getPluginInstance().getControllers().size());
-
-		new A()
-		{
-			@Override
-			public void run()
-			{
-				D.v("Im async!");
-			}
-		};
+		D.s(this, "TPS: " + F.f(ss.getTicksPerSecond(), 2) + " (" + F.time(ss.getTickTime(), 2) + ")");
 	}
 }
